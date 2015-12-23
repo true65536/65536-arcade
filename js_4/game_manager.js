633fafe43ff0bf05ff590f9261995eabe00ff873
Noel Cendana <noel_vivas_cendana@tpsec.edu.sg>
2015-12-23 15:08:06 +0800

    Update game_manager.js

--- js_4/game_manager.js
+++ js_4/game_manager.js
@@ -196,7 +196,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 4) self.won = true;
+          if (merged.value === 0.5) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
