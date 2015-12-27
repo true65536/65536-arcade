512e98efcaf075a110819761a1243c7aa703431a
Noel Cendana <noel_vivas_cendana@tpsec.edu.sg>
2015-12-27 21:46:36 +0800

    Update game_manager_reverse.js

--- js_2048/game_manager_reverse.js
+++ js_2048/game_manager_reverse.js
@@ -40,7 +40,7 @@ GameManager.prototype.setup = function () {
   var previousState = this.storageManager.getGameState();
 
   // Reload the game from a previous game if present
-  if (previousState) {
+  if (1>2) {
     this.grid        = new Grid(previousState.grid.size,
                                 previousState.grid.cells); // Reload grid
     this.score       = previousState.score;
