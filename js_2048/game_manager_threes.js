a99eec0342224213beabd0af9075c77090296e93
Noel Cendana <noel_vivas_cendana@tpsec.edu.sg>
2015-12-27 14:34:11 +0800

    Update game_manager_threes.js

--- js_2048/game_manager_threes.js
+++ js_2048/game_manager_threes.js
@@ -44,7 +44,7 @@ GameManager.prototype.setup = function () {
   var previousState = this.storageManager.getGameState();
 
   // Reload the game from a previous game if present
-  if (previousState && (this.size === previousState.size)) {
+  if (previousState) {
     this.grid        = new Grid(previousState.grid.size,
                                 previousState.grid.cells); // Reload grid
     this.score       = previousState.score;
@@ -61,10 +61,6 @@ GameManager.prototype.setup = function () {
     // Add the initial tiles
     this.addStartTiles();
   }
-
-  // Update the actuator
-  this.actuate();
-};
 
 // Set up the initial tiles to start the game with
 GameManager.prototype.addStartTiles = function () {
