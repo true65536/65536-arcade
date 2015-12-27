c66632927fb72aabede7db332f640c2d289a9997
Noel Cendana <noel_vivas_cendana@tpsec.edu.sg>
2015-12-27 14:32:01 +0800

    Update game_manager_fibonacci.js

--- js_2048/game_manager_fibonacci.js
+++ js_2048/game_manager_fibonacci.js
@@ -36,7 +36,7 @@ GameManager.prototype.setup = function () {
   var previousState = this.storageManager.getGameState();
 
   // Reload the game from a previous game if present
-  if (1>2) {
+  if (previousState) {
     this.grid        = new Grid(previousState.grid.size,
                                 previousState.grid.cells); // Reload grid
     this.score       = previousState.score;
