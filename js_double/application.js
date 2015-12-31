2c0119e260777e23886b357297dc350afe73493e
Noel Cendana <noel_vivas_cendana@tpsec.edu.sg>
2015-12-31 09:46:35 +0800

    Update application.js

--- js_double/application.js
+++ js_double/application.js
@@ -1,5 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   window.gameName = "2048-double";
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  new GameManager(4, KeyboardInputManager, HTMLActuator);
 });
