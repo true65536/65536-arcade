1ae51ed34e89e68e68320824bc6760751c3e6ba8
Noel Cendana <noelcendana@gmail.com>
2016-02-21 03:16:46 +0800

    Update game_manager_finals.js

--- js_2048/game_manager_finals.js
+++ js_2048/game_manager_finals.js
@@ -214,7 +214,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, Goal);
+          var merged = new Tile(positions.next, 2048);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
