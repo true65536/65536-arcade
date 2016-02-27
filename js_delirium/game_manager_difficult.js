a333c62212280876557aa3ebb976732e2c630f38
Noel Cendana <noelcendana@gmail.com>
2016-02-27 18:16:34 +0800

    Update game_manager_difficult.js

--- js_delirium/game_manager_difficult.js
+++ js_delirium/game_manager_difficult.js
@@ -175,7 +175,7 @@ GameManager.prototype.move = function (direction) {
           self.score += Math.pow(2,(merged.value.charCodeAt(0)-62));
 
           // TODO wincheck?
-          if (merged.value === "K") self.won = true;
+          if (merged.value === 0.5) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
