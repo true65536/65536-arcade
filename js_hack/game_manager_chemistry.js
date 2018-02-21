fee38355f6c2c8422ffefd5ca8e39c62a4cf89b9
Noel Cendana <noelcendana@gmail.com>
2018-02-21 20:11:39 +0800

    Update game_manager_chemistry.js

--- js_hack/game_manager_chemistry.js
+++ js_hack/game_manager_chemistry.js
@@ -209,7 +209,7 @@ GameManager.prototype.move = function (direction) {
           tile.updatePosition(positions.next);
 
           // Update the score
-          self.score += merged.value * merged.value + " kJ/mol";
+          self.score += Math.round(Math.random() * 2048) + merged.value * merged.value;
 
           // The mighty 2048 tile
           if (merged.value === 2048) self.won = true;
