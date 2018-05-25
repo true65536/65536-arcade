e1472caa4635ff26437ecf6ac4dc3046b3760b30
Noel Cendana <noelcendana@gmail.com>
2018-05-25 12:01:29 +0800

    Update html_actuator.js

--- js_delirium/html_actuator.js
+++ js_delirium/html_actuator.js
@@ -58,9 +58,10 @@ HTMLActuator.prototype.addTile = function (tile) {
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
   if (tile.value > 2048) classes.push("tile-super");
-  if (tile.value < -2048) classes.push("tile-infinity");
+  if (tile.value < -2048) classes.push("tile--super");
   if (typeof tile.value === "string" && tile.value.charCodeAt(0) > "M".charCodeAt(0)) classes.push("tile-superlative");
-
+  if (tile.value === Infinity) classes.push("tile-infinity");
+  
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
