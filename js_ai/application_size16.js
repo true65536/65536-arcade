6494a559d6efc11bc33f1947662ebb42bf235aae
Noel Cendana <noelcendana@gmail.com>
2018-06-08 17:31:46 +0800

    Update and rename application-size16.js to application_size16.js

--- js_ai/application_size16.js
+++ js_ai/application_size16.js
@@ -1,5 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 var global_game = null;
 window.requestAnimationFrame(function () {
-  global_game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  global_game = new GameManager(16, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
